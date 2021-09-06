e80681505a54c49f01a726659ca4da65276c8de9
Elmo Junior <elmojuniorduarte@gmail.com>
2021-09-05 22:42:42 -0300

    Aula 4

--- src/AppCaneta.java
+++ src/AppCaneta.java
@@ -1,14 +1,14 @@
 public class AppCaneta {
     public static void main(String[] args) throws Exception {
 
-        Caneta c1 = new Caneta();
-        c1.modelo = "BIC";
-        c1.cor = "Amarelo";
-        // c1.ponta = 0.44f; 
-        c1.carga = 80;
-        // c1.tampada = false;
-        c1.destampar();
+        Caneta c1 = new Caneta("BIC", "Amarelo", 0.44f);
+        // c1.setModelo("BIC");
+        // c1.setPonta(0.44f);
         c1.status();
-        c1.rabiscar();
+
+        // System.out.println("Uma caneta " + c1.getModelo() + " de ponta " + c1.getPonta());
+        
+        Caneta c2 = new Caneta("Stablo", "Preta", 0.44f);
+        c2.status();
     }
 }
--- src/Caneta.java
+++ src/Caneta.java
@@ -1,3 +1,5 @@
+import java.util.Set;
+
 public class Caneta {
     public String modelo, cor;
     private float ponta;
@@ -12,13 +14,58 @@ public class Caneta {
         System.out.println("Tampada: " + this.tampada);
     }
 
+    public Caneta(){
+
+    }
+    
+    public Caneta(String m, String c, float p) {
+        this.setModelo(m);
+        this.setCor(c);
+        this.setPonta(p);
+        this.tampar();
+    }
+
+    public String getModelo(){
+        return this.modelo;
+    }
+    public void setModelo(String m){
+        this.modelo = m;
+    }
+
+    public String getCor() {
+        return this.cor;
+    }
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public float getPonta(){
+        return this.ponta;
+    }
+    public void setPonta(float p){
+        this.ponta = p;
+    }
+
+    public int getCarga() {
+        return this.carga;
+    }
+    public void setCarga(int carga) {
+        this.carga = carga;
+    }
+
+    public boolean isTampada() {
+        return this.tampada;
+    }
+    public void setTampada(boolean tampada) {
+        this.tampada = tampada;
+    }
+
     public void rabiscar(){
         if (this.tampada==true) {
             System.out.println("Sorry, não posso rabiscar estando tampada");
         } else {
             System.out.println("Rabiscando");
         }
-
     }
 
     public void tampar(){
@@ -28,4 +75,5 @@ public class Caneta {
     public void destampar(){
         this.tampada = false;
     }
+
 }
